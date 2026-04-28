43a727b8770269ed664b96a51a527454fd2c18d8
Luka Vukovic <vukovicluka33@gmail.com>
2026-04-29 00:00:17 +0200

    Prikaz modala za potvrdu otkazivanja rezervacije

--- src/components/termini/Termin.tsx
+++ src/components/termini/Termin.tsx
@@ -1,4 +1,11 @@
-import { Avatar, Button, Card, Flex } from "antd";
+import {
+  Avatar,
+  Button,
+  Card,
+  Flex,
+  Popconfirm,
+  type PopconfirmProps,
+} from "antd";
 import type { MouseEvent } from "react";
 import type { Termin } from "../../types";
 import { formatirajVrijemeTreninga } from "../../helpers/formatirajVrijemeTreninga";
@@ -27,81 +34,94 @@ export default function TerminCard({ termin }: TerminProps) {
     });
   };
 
-  const otkaziRezervaciju = (e: MouseEvent<HTMLElement>) => {
-    e.preventDefault();
+  const otkaziRezervaciju = (e?: MouseEvent<HTMLElement>) => {
+    e?.preventDefault();
     mutateOtkazi({
       terminId: termin._id,
-      userId: "123"
+      userId: "123",
     });
   };
 
+  const confirm: PopconfirmProps["onConfirm"] = (e) => otkaziRezervaciju(e);
+
   const popunjenTermin = termin.brojRezervacija >= termin.brojMjesta;
-  const uTijekuAkcija = isPendingRezerviraj || isPendingOtkazi; 
+  const uTijekuAkcija = isPendingRezerviraj || isPendingOtkazi;
   return (
-    <NavLink
-      to={`/termin/${termin._id}`}
-      title="Vidi detalje"
+    <Card
+      title={
+        <NavLink
+          to={`/termin/${termin._id}`}
+          title="Vidi detalje"
+          style={{ display: "block"}}
+        >
+          {termin.naziv}
+        </NavLink>
+      }
+      variant="borderless"
+      style={{ width: 300 }}
     >
-      <Card
-        title={termin.naziv}
-        variant="borderless"
-        style={{ width: 300 }}
+      <Flex
+        vertical
+        align="start"
+        gap={5}
       >
+        <IconWrapper>
+          <Avatar size={30}>{termin.idTrenera}</Avatar>
+          <p>{termin.idTrenera}</p>
+        </IconWrapper>
         <Flex
-          vertical
-          align="start"
+          gap={10}
+          align="center"
+        >
+          <IconWrapper>
+            <CiClock1 size={15} />
+            {termin.trajanjeMin} min
+          </IconWrapper>
+          <IconWrapper>
+            <FaPeopleGroup size={15} />
+            {termin.brojRezervacija}/{termin.brojMjesta}
+          </IconWrapper>
+        </Flex>
+        <Flex
+          align="center"
           gap={5}
         >
           <IconWrapper>
-            <Avatar size={30}>{termin.idTrenera}</Avatar>
-            <p>{termin.idTrenera}</p>
+            <IoCalendarOutline size={15} />
+            {formatirajVrijemeTreninga(termin.vrijeme)}
           </IconWrapper>
-          <Flex
-            gap={10}
-            align="center"
+        </Flex>
+        <IconWrapper>
+          <Button
+            onClick={rezervirajTermin}
+            loading={uTijekuAkcija}
+            disabled={popunjenTermin || termin.userRezervirao}
           >
-            <IconWrapper>
-              <CiClock1 size={15} />
-              {termin.trajanjeMin} min
-            </IconWrapper>
-            <IconWrapper>
-              <FaPeopleGroup size={15} />
-              {termin.brojRezervacija}/{termin.brojMjesta}
-            </IconWrapper>
-          </Flex>
-          <Flex
-            align="center"
-            gap={5}
-          >
-            <IconWrapper>
-              <IoCalendarOutline size={15} />
-              {formatirajVrijemeTreninga(termin.vrijeme)}
-            </IconWrapper>
-          </Flex>
-          <IconWrapper>
-            <Button
-              onClick={rezervirajTermin}
-              loading={uTijekuAkcija}
-              disabled={popunjenTermin || termin.userRezervirao}
+            {termin.userRezervirao
+              ? "Rezervirano"
+              : !popunjenTermin
+                ? "Rezerviraj"
+                : "Popunjeno"}
+          </Button>
+          {termin.userRezervirao && (
+            <Popconfirm
+              title="Otkazivanje rezervacije"
+              description="Jeste li sigurni da želite otkazati rezervaciju?"
+              onConfirm={confirm}
+              okText="Da"
+              cancelText="Ne"
+              placement="bottom"
             >
-              {termin.userRezervirao
-                ? "Rezervirano"
-                : !popunjenTermin
-                  ? "Rezerviraj"
-                  : "Popunjeno"}
-            </Button>
-            {termin.userRezervirao && (
               <Button
                 loading={uTijekuAkcija}
-                onClick={otkaziRezervaciju}
                 type="text"
                 icon={<MdOutlineDeleteForever size={25} />}
                 title="Otkaži rezervaciju"
               />
-            )}
-          </IconWrapper>
-        </Flex>
-      </Card>
-    </NavLink>
+            </Popconfirm>
+          )}
+        </IconWrapper>
+      </Flex>
+    </Card>
   );
 }
